1d49a3d88f2e438b2e6cbc8df3cbcdae925b97e3
Gleb Zavizenov <glebzavizenov@Glebs-Mac-mini.local>
2019-06-03 20:37:08 -0400

    initial project setup

---
+++ Smack/Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Gleb Zavizenov on 2019-06-03.
+//  Copyright © 2019 Gleb Zavizenov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
